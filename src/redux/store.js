cdf8ee91b492c48a559456285bf04cb3f86a70a4
thai.vo <43738511+ht0201@users.noreply.github.com>
2022-10-16 17:23:36 +0700

    add silce to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filtersSlice } from '../components/Filters/filtersSilce';
+import { todosSlice } from '../components/TodoList/todosSlice';
 
 export const store = configureStore({
   reducer: {
     filters: filtersSlice.reducer,
-    todoList: [],
+    todoList: todosSlice.reducer,
   },
 });
